cc1512a127f1e51ceb3a6c352c85915799b27472
jidris-spec <82263263+jidris-spec@users.noreply.github.com>
2022-04-13 23:21:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mad_Libs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Pronoun, colour, celebrity;
+
+            Console.Write("enter a pronoun : ");
+             Pronoun = Console.ReadLine();
+            Console.Write("enter a colour : ");
+            colour = Console.ReadLine();
+            Console.Write(" enter a celebrity : ");
+            celebrity = Console.ReadLine();
+            Console.Write(Pronoun + " is a Genius ");
+            Console.Write(" with a " + colour + " hair");
+            Console.Write(" I really admire my idol  " + celebrity);
+
+            
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
